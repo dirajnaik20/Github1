43dbf0eaeea59759e461adc4b0a626590649b5d7
diraj <diraj@192.168.1.41>
2022-04-25 15:50:44 +0530

    Commit1

---
+++ githubpract/src/githubpract/Orange.java
@@ -0,0 +1,17 @@
+package githubpract;
+
+public class Orange {
+  String color;
+Orange(String color)
+{
+		this.color=color;
+}
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	
+	Orange greenApple = new Orange("Orange");
+	String color = greenApple.getColor();
+	System.out.println(&quot;color of apple is &quot; + color);
+
+}
+}
